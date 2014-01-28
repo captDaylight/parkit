50a846f87274917666cecc255a2be82d785922aa
Paul Christophe <paulchristophe@pauls-air.intern>
2014-01-28 08:50:24 -0500

    let's see if this works

--- park/settings.py
+++ park/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'park_app',
 )
 
 MIDDLEWARE_CLASSES = (
--- park/urls.py
+++ park/urls.py
@@ -2,10 +2,13 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
+from park_app import views
+
+print(views)
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'park.views.home', name='home'),
+    url(r'^$', views.index, name='index'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
