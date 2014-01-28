2081211da933acac879574cadbbb61532e374567
Paul Christophe <paulchristophe@pauls-air.intern>
2014-01-28 15:14:37 -0500

    images for display

--- park/urls.py
+++ park/urls.py
@@ -8,7 +8,8 @@ from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', views.index, name='index'),
+    url(r'^howto/$', views.index, name='index'),
+    url(r'^$', views.gallery, name=''),
     # url(r'^blog/', include('blog.urls')),
     # url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT }),
 
--- park_app/views.py
+++ park_app/views.py
@@ -11,7 +11,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.context_processors import csrf
 
 def index(request):
-    return render_to_response('park/index.html', context_instance=RequestContext(request))
+	return render_to_response('park/index.html', context_instance=RequestContext(request))
 
 def gallery(request):
     return render_to_response('park/gallery.html', context_instance=RequestContext(request))
